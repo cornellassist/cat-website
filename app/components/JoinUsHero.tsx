c70df69b9eedd80674803d47a6a443834e421a69
brianxia273 <briangxia@gmail.com>
2026-02-01 02:37:44 -0500

    photo size adjustmnets

--- app/components/JoinUsHero.tsx
+++ app/components/JoinUsHero.tsx
@@ -10,7 +10,7 @@ function HeroContent() {
       <h1 className="mainheading mt-10">Join Us</h1>
       <p className="subtext mt-4 w-full text-center">We will not be recruiting Spring 2026, but applications will reopen Fall 2026. Thank you for your interest, and we look forward to your applications!</p>
       <Image src={HeroPic} alt="A Cornell Assistive Technology team work session" height={HeroPic.height} width={HeroPic.width}
-        className="mt-5 h-80 origin-top scale:90 sm:scale-120 lg:scale-100 w-auto rounded-[20px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]" />
+        className="mt-5 h-80 origin-top scale:90 sm:scale-120 xl:scale-130 2xl:scale-140 w-auto rounded-[20px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]" />
       <div className="mt-12 flex justify-center">
         {/* <ButtonRed label="Apply Now" to="" behav="External" disabled={true} /> */}
       </div>
@@ -21,7 +21,7 @@ function HeroContent() {
 
 const HeroContainer = ({ children }: any) => {
   return (
-    <div className="mt-24 universepad relative overflow-hidden h-150 sm:h-180 md:h-180 lg:h-155 xl:h-170 2xl:h-190">
+    <div className="mt-24 universepad relative overflow-hidden h-150 sm:h-180 md:h-180 lg:h-190 2xl:h-200">
       <Image src={BlurTL} alt="" height={BlurTL.height / 2} width={BlurTL.width / 2} className="absolute left-0 top-0 -z-10" />
       <Image src={BlurTR} alt="" height={BlurTR.height} width={BlurTR.width} className="absolute right-0 top-0 -z-10" />
       {children}
