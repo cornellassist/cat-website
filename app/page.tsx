22407d3019c8a669d66d72f3581fa745bb81df26
Brian Xia <briangxia@gmail.com>
2026-02-13 13:08:02 -0500

    Speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 import Home from "./Home/page";
-import { Analytics } from "@vercel/analytics/next"
-
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function HomePage() {
   return (
     <>
       <Home />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
