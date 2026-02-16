978f288944f9b6cb8f899ac23855239ce064491b
Brian Xia <briangxia@gmail.com>
2026-02-16 15:46:05 -0500

    no types

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,8 +1,7 @@
 import prisma from "@/utils/prisma";
-import type { Event as EventType } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const events: EventType[] = await prisma.event.findMany();
+  const events = await prisma.event.findMany();
   return NextResponse.json(events);
 }
--- app/api/highlights/route.ts
+++ app/api/highlights/route.ts
@@ -1,9 +1,7 @@
 import prisma from "@/utils/prisma";
-import type { CommunityHighlights as CommunityHighlightType } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const highlights: CommunityHighlightType[] =
-    await prisma.communityHighlights.findMany();
+  const highlights = await prisma.communityHighlights.findMany();
   return NextResponse.json(highlights);
 }
