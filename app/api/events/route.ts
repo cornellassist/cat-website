a040202cd1212bb3e64f06ff32be9dcc4fcf3724
Brian Xia <briangxia@gmail.com>
2026-04-16 12:40:34 -0400

    Update route.ts

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
-  const supabase = await createClient();
+  const supabase = await createClient(); // so that cookies are tied with incoming request
   const { data } = await supabase.auth.getUser();
 
   if (!data.user) {
@@ -19,23 +19,41 @@ export async function POST(request: NextRequest) {
   }
 
   const body = await request.json();
-
-  await prisma.event.create({
-    data: {
-      id: 1,
-      title: "hi",
-      tags: ["Kids"],
-      descrip: "hi",
-      date: "date",
-      time: "time",
-      location: "location",
-      imageUrl: "image",
-    },
-  });
-
-  return NextResponse.json({ success: true }, { status: 200 });
+  try {
+    const event = await prisma.event.create({
+      // use same central prisma object, not dependent per-request
+      data: {
+        id: 1,
+        title: "hi",
+        tags: ["Kids"],
+        descrip: "hi",
+        date: "date",
+        time: "time",
+        location: "location",
+        imageUrl: "image",
+      },
+    });
+    console.log(event);
+    return NextResponse.json({ success: true }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
 
-export async function DELETE() {
+export async function DELETE(request: NextRequest) {
   const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized user" }, { status: 401 });
+  }
+  const body = await request.json();
+
+  try {
+    const deletedEvent = prisma.event.delete({ where: { id: 100 } }); // placeholder id
+    console.log(deletedEvent);
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
