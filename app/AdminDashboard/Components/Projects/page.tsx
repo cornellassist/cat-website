cceaca30a3bba9c0b819aa9219bb87a2d589f1b8
Brian Xia <briangxia@gmail.com>
2026-04-19 15:03:51 -0400

    updated sidebar

---
+++ app/AdminDashboard/Components/Projects/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Sidebar } from "@/app/components/Sidebar";
+
+export default function Projects() {
+  return <Sidebar />;
+}
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,38 +1,100 @@
-import { useState } from "react";
 import { HomeIcon } from "@heroicons/react/24/solid";
-import { SquaresPlusIcon } from "@heroicons/react/24/solid";
-import { PhotoIcon } from "@heroicons/react/24/solid";
-import { UserGroupIcon } from "@heroicons/react/24/solid";
+import {
+  SquaresPlusIcon,
+  UserGroupIcon,
+  PhotoIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from "@heroicons/react/24/solid";
 import { createClient } from "@/utils/supabase/client";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const iconStyles = "h-5 w-5";
 
 export function Sidebar() {
+  const path = usePathname();
+  const router = useRouter();
   const supabase = createClient();
+
   type TabProp = {
     label: string;
     icon: any;
     isActive?: boolean;
+    dropdown?: string[];
   };
 
   const tabs: TabProp[] = [
     { label: "Home", icon: HomeIcon },
-    { label: "Components", icon: SquaresPlusIcon },
+    {
+      label: "Components",
+      icon: SquaresPlusIcon,
+      dropdown: [
+        "Projects",
+        "Community Highlights",
+        "Events",
+        "Sponsors",
+        "Members",
+      ],
+    },
     { label: "Images", icon: PhotoIcon },
     { label: "Team", icon: UserGroupIcon },
   ];
 
-  const [activeTab, setActiveTab] = useState<string>("Home");
-
-  function Tab({ label, icon, isActive }: TabProp) {
+  function Tab({ label, icon, isActive, dropdown }: TabProp) {
+    const [dropOn, setDropOn] = useState<boolean>(path.includes(label));
     const Icon = icon;
     return (
-      <div
-        className={`flex items-center gap-4 h-12 ${isActive ? "bg-text-lt-grey" : "bg-transparent"} cursor-pointer pl-5 mx-1.5 rounded-sm`}
-        onClick={() => {
-          setActiveTab(label);
-        }}
-      >
-        <Icon className="h-5 w-5" />
-        <div>{label}</div>
+      <div className="flex flex-col mx-1.5 select-none ">
+        <div
+          className={`px-5 flex items-center justify-between h-12 ${isActive ? "bg-text-lt-grey" : "bg-transparent"} cursor-pointer rounded-sm`}
+          onClick={() => {
+            if (!dropdown) {
+              const routeToPath =
+                label === "Home"
+                  ? "/AdminDashboard"
+                  : `/AdminDashboard/${label}`;
+              router.push(routeToPath);
+            } else {
+              setDropOn(!dropOn);
+            }
+          }}
+        >
+          <div className="flex items-center gap-2">
+            <Icon className={iconStyles} />
+            <div>{label}</div>
+          </div>
+          {dropdown &&
+            (!dropOn ? (
+              <ChevronDownIcon className={iconStyles} />
+            ) : (
+              <ChevronUpIcon className={iconStyles} />
+            ))}
+        </div>
+        {dropdown && (
+          <div
+            className={`flex flex-col gap-4 transition-all duration-300 ease-in-out overflow-hidden ${
+              dropOn ? "max-h-96 mt-2 opacity-100" : "max-h-0 opacity-0"
+            }`}
+          >
+            {dropdown.map((entry, index) => {
+              return (
+                <div
+                  className={`pl-12 pr-5 descriptext ${path.includes(entry) ? "bg-text-lt-grey" : "bg-transparent"} rounded-sm`}
+                  key={index}
+                  onClick={() => {
+                    router.push(
+                      `AdminDashboard/${label}/${entry.replaceAll(" ", "")}`,
+                    );
+                  }}
+                >
+                  {entry}
+                </div>
+              );
+            })}{" "}
+          </div>
+        )}
       </div>
     );
   }
@@ -44,24 +106,19 @@ export function Sidebar() {
     >
       <div className="flex flex-col gap-6 w-full mt-30">
         {tabs.map((tab, index) => {
+          const splitPath = path.split("/");
+          const curPath = splitPath.length > 2 ? splitPath.pop() : "Home";
+          console.log(splitPath);
           return (
             <Tab
               label={tab.label}
               icon={tab.icon}
               key={index}
-              isActive={tab.label === activeTab}
+              isActive={tab.label === curPath}
+              dropdown={tab?.dropdown}
             />
           );
         })}
-      </div>
-
-      <div
-        className="bg-amber-200 cursor-pointer"
-        onClick={() => {
-          supabase.auth.signOut();
-        }}
-      >
-        Dev signout button
       </div>
     </div>
   );
